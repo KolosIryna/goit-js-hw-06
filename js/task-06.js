e9f3326e0d16655456a28f17ee8d9f71dc90eff1
KolosIryna <kolos.irina23@gmail.com>
2023-07-18 18:06:10 +0300

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,9 @@ inputEl.addEventListener("blur", onInputBlur);
 function onInputBlur (event) {
     if (event.currentTarget.value.length === inputLength) {
         inputEl.classList.add("valid");
+        inputEl.classList.remove("invalid");
     } else {
+        inputEl.classList.remove("valid");
         inputEl.classList.add("invalid");
     }
 }
